020e518f264b5b081e8fded0f450c55b5628198d
Fahid Hossain <fahidhossain2021@gmail.com>
2022-01-04 02:54:03 +0600

    footer design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home/Home';
+import Footer from './pages/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Home></Home>
+      <Footer></Footer>
     </div>
   );
 }
