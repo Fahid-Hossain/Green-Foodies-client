8bb0d00433ef5adf6f1477caa1711375d0dfcdfe
mizan <dewan.mizanur911@gmail.com>
2022-01-04 11:57:24 +0600

    register page design completed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home/Home';
 import Footer from './pages/Footer/Footer';
 import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 
 function App() {
   return (
     <div className="App">
       <Home></Home>
       <Login></Login>
+      <Register></Register>
       <Footer></Footer>
     </div>
   );
