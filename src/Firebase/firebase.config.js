e394aaca03affaac38521cc3918ff13955b8df61
Fahid Hossain <fahidhossain2021@gmail.com>
2022-01-04 13:26:04 +0600

    firebasee setup

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCkr_N-IH_RCho0ubF2-KTSG89hQjqy6sA",
+    authDomain: "green-foodies-d7b27.firebaseapp.com",
+    projectId: "green-foodies-d7b27",
+    storageBucket: "green-foodies-d7b27.appspot.com",
+    messagingSenderId: "966604876685",
+    appId: "1:966604876685:web:c0fa848098dcdf11c22bc4"
+  };
+
+  export default firebaseConfig;
