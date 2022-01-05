63b030910887e9a71a9cc942bc2b21fb3d892965
mizan <dewan.mizanur911@gmail.com>
2022-01-06 03:52:49 +0600

    changes done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home/Home';
 import Footer from './pages/Footer/Footer';
 import AuthProvider from './context/AuthProvider';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 
 function App() {
   return (
     <div className="App">
       <AuthProvider>
       <Home></Home>
+      <Login></Login>
+      <Register></Register>
       <Footer></Footer>
       </AuthProvider>
     </div>
