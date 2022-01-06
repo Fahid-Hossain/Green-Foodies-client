51b2852951daad8e0d77a3720cb4c8ba84b63e97
Fahid Hossain <fahidhossain2021@gmail.com>
2022-01-06 07:21:29 +0600

    rating setup and design

--- src/pages/Foods/Foods.js
+++ src/pages/Foods/Foods.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Col, Row, Spinner } from 'react-bootstrap';
+import { Button, Card, Col, Row, Spinner } from 'react-bootstrap';
+import Rating from 'react-rating';
+import { Link } from 'react-router-dom';
+import "./Food.css"
 
 const Foods = () => {
     const [foods, setFoods] = useState([]);
@@ -15,24 +18,33 @@ const Foods = () => {
         <div>
             <h2>Our Stock Now {foods.length}</h2>
             {
-                foods.length ===0 && <Spinner animation="border" variant="success" />
+                foods.length === 0 && <Spinner animation="border" variant="success" />
             }
             {
-                foods.length !==0 && <Row xs={1} md={3} className="g-4">
-                {foods.map((food, idx) => (
-                    <Col>
-                        <Card>
-                            <Card.Img variant="top" src={food.image} />
-                            <Card.Body>
-                                <Card.Title>{food.title}</Card.Title>
-                                <Card.Text>
-                                    {food.description}
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                ))}
-            </Row>
+                foods.length !== 0 && <Row xs={1} md={3} className="g-0">
+                    {foods.map((food, idx) => (
+                        <Col>
+                            <Card>
+                                <Card.Img className="food-img" variant="top" src={food.image} />
+                                <Card.Body>
+                                    <Card.Title>{food.title}</Card.Title>
+                                    <h3>${food.price}</h3>
+                                    <h6>{food.category}</h6>
+                                    <Card.Text>
+                                        {food.description}
+                                    </Card.Text>
+                                    <Rating className="rating"
+                                        initialRating={food.rating.rate}
+                                        readonly
+                                        emptySymbol="far fa-star"
+                                        fullSymbol="fas fa-star"
+                                    />
+                                </Card.Body>
+                                    <Link to="/"><Button className="food-order-btn" variant="success" size="lg">Order-now</Button>{' '}</Link>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
             }
         </div>
     );
