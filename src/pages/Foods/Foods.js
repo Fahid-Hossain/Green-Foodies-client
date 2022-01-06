ecd01a4ddcfb412357a5d859bc6d5aa742c92ae9
Fahid Hossain <fahidhossain2021@gmail.com>
2022-01-06 06:34:13 +0600

    load data from database and display on UI

--- src/pages/Foods/Foods.js
+++ src/pages/Foods/Foods.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Card, Col, Row, Spinner } from 'react-bootstrap';
 
 const Foods = () => {
+    const [foods, setFoods] = useState([]);
+    useEffect(() => {
+        fetch("https://fast-caverns-43856.herokuapp.com/foods")
+            .then(res => res.json())
+            .then(data => {
+                setFoods(data);
+            })
+    }, [])
+
     return (
         <div>
-            <h2>foods</h2>
+            <h2>Our Stock Now {foods.length}</h2>
+            {
+                foods.length ===0 && <Spinner animation="border" variant="success" />
+            }
+            {
+                foods.length !==0 && <Row xs={1} md={3} className="g-4">
+                {foods.map((food, idx) => (
+                    <Col>
+                        <Card>
+                            <Card.Img variant="top" src={food.image} />
+                            <Card.Body>
+                                <Card.Title>{food.title}</Card.Title>
+                                <Card.Text>
+                                    {food.description}
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+            }
         </div>
     );
 };
